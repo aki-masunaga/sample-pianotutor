fc85b930545d75dc8e00414bd50e6e4b6fb141f0
aki-masunaga <108311811+aki-masunaga@users.noreply.github.com>
2023-01-09 16:00:54 +0900

    Create script.js
    JSファイルを追加しました

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    /* スライダー（Slick）  */
+    $('.slider').slick({
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pouseOnHover: false,
+      fade: true
+    });
+
+});
